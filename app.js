8aad448173dae75446cd0941f8bc2fdc8ea2d23d
Eduard Metzler <eduard.metzler@mail.ru>
2021-04-20 17:16:05 +0200

    first start

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json({ extended: true }));
+
+
+
+mongoose.set("useFindAndModify", false);
+
+
+
+// app.use("/api/auth", require("./routes/auth.routes"));
+
+const PORT = config.get("port") || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    app.listen(PORT, () =>
+      console.log(`App has been started on port ${PORT}.......... `)
+    );
+  } catch (e) {
+    process.exit(1);
+  }
+}
+
+start();
